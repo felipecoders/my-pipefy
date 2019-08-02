fed22d76479d303ca1746d269bda7a23f189d113
Felipe de Oliveira Ribeiro <felipeoliveirasl@hotmail.com>
2019-08-01 23:17:32 -0300

    initial commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 0 30px;
+  background: #c2185b;
+  color: #fff;
+
+  display: flex;
+  align-items: center;
+`;
